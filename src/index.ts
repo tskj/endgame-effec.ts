73feb8cb1140a77c4b394407788c9eca26583cd4
tskj <epost@tarjei.org>
2024-11-09 18:37:53 +0100

    change api to only use with({...})

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 type Handler = <Input>(p: Program<Input>) => Program<Input>;
 
 type Program<Input> = {
-  with: (h: Handler) => Program<Input>;
+  with: (h: Handlers) => Program<Input>;
+  withOverride: (h: Handlers) => Program<Input>;
   run: (x: Input, history?: any[]) => void;
 }
 
@@ -13,17 +14,9 @@ type Handlers = {
   return?: (v: unknown) => void,
 }
 
-const handlers = (hs: Handlers): Handler => {
-  const handler = <Input>(p: Program<Input>) => {
-    return {
-      with: (h: Handler) => h(p),
-      run: (x: any, history: any) => p.run(x, history),
-    }
-  };
-  return handler as Handler;
-};
-
 const program = <Input>(g: (x: Input) => Generator<Input>): Program<Input> => {
+  let handlers: any = {};
+
   const run = (x: Input, history: any[] = []) => {
     const it = g(x);
     let state = it.next();
@@ -32,16 +25,39 @@ const program = <Input>(g: (x: Input) => Generator<Input>): Program<Input> => {
       state = it.next(x);
     });
 
-    if (state.done) {
-      throw ["return", state.value];
+    if (!state.done) {
+      const value = state.value;
+      if (!Array.isArray(value)) throw "expected yielded value to be a tuple";
+      if (value.length !== 2) throw "expected yielded value to be a 2-tuple";
+
+      const [handlerKey, args] = value;
+      const handler = handlers[handlerKey];
+      if (!handler) throw "expected to find handler"
+
+      const k = program(function* () {
+
+      }).with(handlers);
+      const r = ''
+
+      handler(...args)(k, r);
     } else {
-      // chain run(x, [...history, state.value])
-      throw state.value;
+      const returnValue = state.value;
+      const returnHandler = handlers['return'];
+      if (returnHandler) returnHandler(returnValue);
     }
   }
 
   const p: Program<Input> = {
-    with: (h: Handler) => h(p),
+    with: (h: Handlers) => {
+      handlers = { ...h, ...handlers };
+      // needs to be a new p really
+      return p;
+    },
+    withOverride: (h: Handlers) => {
+      handlers = { ...handlers, ...h };
+      // needs to be a new p really
+      return p;
+    },
     run,
   };
 
@@ -51,12 +67,12 @@ const program = <Input>(g: (x: Input) => Generator<Input>): Program<Input> => {
 const x = program(function* () {
   // testing!
 })
-.with(handlers({
+.with({
   call: (fn, ...args) => (k, r) => {
-    fn.with(handlers({
-      return: v => () => {
-         k.with(handlers({ return: r })).run(v);
+    fn.with({
+      return: (v: any) => () => {
+         k.with({ return: r }).run(v);
       },
-    })).run(...args);
+    }).run(...args);
   },
-}));
+});
